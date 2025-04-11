e09b7685104b685c41746cf473e802a9a96f4c77
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-04-11 13:08:58 +0900

    get-userid로 검색하는 기능 #3

--- src/main/java/com/example/backend/controller/BookController.java
+++ src/main/java/com/example/backend/controller/BookController.java
@@ -1,9 +1,18 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.BookDTO;
+import com.example.backend.dto.ResponseDTO;
+import com.example.backend.model.BookEntity;
 import com.example.backend.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("book") //localhost:8080/book
@@ -11,4 +20,15 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    //userID 가 가지고 있는 모든 도서 데이터를 반환
+    @GetMapping()
+    public ResponseEntity<?> getBookList(){
+        String temporaryUserId = "KimJinSeon";
+
+        List<BookEntity> bookEntityList = bookService.getBookList(temporaryUserId);
+        List<BookDTO> bookDTOList = bookEntityList.stream().map(BookDTO::new).collect(Collectors.toList());
+        ResponseDTO<BookDTO> responseDTO = ResponseDTO.<BookDTO>builder().data(bookDTOList).build();
+        return ResponseEntity.ok().body(responseDTO);
+    }
 }
--- src/main/java/com/example/backend/service/BookService.java
+++ src/main/java/com/example/backend/service/BookService.java
@@ -1,9 +1,12 @@
 package com.example.backend.service;
 
+import com.example.backend.model.BookEntity;
 import com.example.backend.persistence.BookRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,4 +16,7 @@ public class BookService {
     private BookRepository bookRepository;
 
 
+    public List<BookEntity> getBookList(final String temporaryUserId) {
+        return bookRepository.findByUserId(temporaryUserId);
+    }
 }
