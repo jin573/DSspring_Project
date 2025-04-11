ad27247967d134088a39bfb0473c1d7e7eea2edb
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-04-11 16:30:42 +0900

    특정 제품 조회 기능 #3

--- src/main/java/com/example/backend/controller/BookController.java
+++ src/main/java/com/example/backend/controller/BookController.java
@@ -46,13 +46,32 @@ public class BookController {
 
     //userID 가 가지고 있는 모든 도서 데이터를 반환
     @GetMapping
-    public ResponseEntity<?> getBookList(){
+    public ResponseEntity<?> getAllBookList(){
         String temporaryUserId = "KimJinSeon";
         //userid 로 저장된 book entity 검색
-        List<BookEntity> bookEntityList = bookService.getBookList(temporaryUserId);
+        List<BookEntity> bookEntityList = bookService.getAllBookList(temporaryUserId);
         //response body에 담기 위해 dto로 변경
         List<BookDTO> bookDTOList = bookEntityList.stream().map(BookDTO::new).collect(Collectors.toList());
         ResponseDTO<BookDTO> responseDTO = ResponseDTO.<BookDTO>builder().data(bookDTOList).build();
         return ResponseEntity.ok().body(responseDTO);
+    }
+
+    //전체 데이터가 아닌 특정 book dto를 검색했을 때 반환
+    @GetMapping("/search/{bookTitle}")
+    public ResponseEntity<?> getBookToList(@PathVariable String bookTitle){
+        /* 클라이언트가 book title을 검색 시
+        * 해당하는 dto 객체들 responseEntity 에 담아 반환한다.
+        * 동일한 책 제목의 다른 출반사가 있을 수 있으므로, list 형태로 반환한다.
+        * userid 로 저장된 book entity 검색*/
+        String temporaryUserId = "KimJinSeon";
+        List<BookEntity> bookEntityList = bookService.getBookToList(temporaryUserId, bookTitle); //service에서 검색하여 entity에 반환
+
+        /*responseEntity에 담아 반환하기 위해
+        * 1. bookEntity를 bookDTO로 변환
+        * 2. bookDTO를 responseDTO에 담음
+        * 3. responseEntity에 responseDTO를 담아 반환한다.*/
+        List<BookDTO> bookDTOList = bookEntityList.stream().map(BookDTO::new).collect(Collectors.toList()); //1
+        ResponseDTO<BookDTO> responseDTO = ResponseDTO.<BookDTO>builder().data(bookDTOList).build(); //2
+        return ResponseEntity.ok().body(responseDTO); //3
     }
 }
